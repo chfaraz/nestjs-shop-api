ed44a037c3cee2e840e9dd55d9d124f839cd0f1f
Ch Faraz Ahmed <38011901+faraz3@users.noreply.github.com>
2021-06-03 12:09:01 +0500

    removed uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: 'mongodb+srv://faraz:12345@cluster0.vk5jz.mongodb.net/newshopgql?retryWrites=true&w=majority',
+      url: '',
       synchronize: true,
       useUnifiedTopology: true,
       entities: [User, Item],
